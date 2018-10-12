f9ca3fa98afac89f7db7c4f4b8a46dfc1088ff44
s-daiki <34276846+s-daiki@users.noreply.github.com>
2018-10-12 11:44:39 +0900

    Update head.php

--- head.php
+++ head.php
@@ -3,7 +3,7 @@
 
   <head>
   <meta charset="utf-8">
-  <title><?php echo $siteTitle; ?> | WEBUKATU MARKET</title>
+  <title><?php echo $siteTitle; ?> | MARKET</title>
   <link rel="stylesheet" type="text/css" href="style.css">
   <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
   <!-- フォントアイコン -->
